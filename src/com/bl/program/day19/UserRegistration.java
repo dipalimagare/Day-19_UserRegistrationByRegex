bb5e8d7e284dd1197175bba8db2b78697d9c27bb
dipalimagare <kalpeshmahajan90@gmail.com>
2021-08-21 09:39:32 +0530

    Uploading Regex Problem

---
+++ src/com/bl/program/day19/UserRegistration.java
@@ -0,0 +1,16 @@
+package com.bl.program.day19;
+
+import RegexUtility.Utility;
+
+public class UserRegistration {
+    public static void main(String[] args) {
+        Utility utility = new Utility();
+
+        utility.firstName();
+        utility.lastName();
+        utility.email();
+        utility.contactNumber();
+        utility.password();
+
+    }
+}
